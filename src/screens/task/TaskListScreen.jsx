1371e887f72bd9bcf1a6de81cbaa9f93f38d6360
devsly <68057615+devsly@users.noreply.github.com>
2026-02-05 13:00:40 +0200

    Update TaskListScreen with UI

--- src/screens/task/TaskListScreen.jsx
+++ src/screens/task/TaskListScreen.jsx
@@ -1,9 +1,20 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 
-export default function TaskListScreen() {
+
+
+export default function TaskListScreen({ navigation }) {
+
   return (
-    <View>
-      <Text>Task List Screen</Text>
+    <View >
+      <Text >My Tasks</Text>
+
+      <Text>All tasks shown here</Text>
+
+      {/* Add new task */}
+      <Pressable onPress={() => navigation.navigate('AddTask')} >
+        <Text >＋</Text>
+      </Pressable>
     </View>
-  )
+  );
 }
+
