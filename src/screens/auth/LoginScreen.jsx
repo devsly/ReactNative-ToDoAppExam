e800a3fd2cca973496eaa576deecf091dd415cbe
devsly <68057615+devsly@users.noreply.github.com>
2026-02-05 09:56:46 +0200

    Update LoginScreen with UI

--- src/screens/auth/LoginScreen.jsx
+++ src/screens/auth/LoginScreen.jsx
@@ -1,9 +1,36 @@
-import { View, Text } from 'react-native';
+import { View,  Text,  TextInput,  Pressable,  KeyboardAvoidingView} from 'react-native';
 
-export default function LoginScreen() {
+export default function LoginScreen({ navigation }) {
   return (
-     <View>
-      <Text>Login Screen</Text>
-    </View>
-  )
+    <KeyboardAvoidingView >
+
+      <Text >Welcome Back</Text>
+      <Text >Login to your account</Text>
+
+      <View >
+        <Text >Email</Text>
+        <TextInput
+          placeholder="Enter your email"
+          keyboardType="email-address"
+        />
+      </View>
+
+      <View >
+        <Text >Password</Text>
+        <TextInput
+          placeholder="Enter your password"
+        />
+      </View>
+
+      <Pressable >
+        <Text >Login</Text>
+      </Pressable>
+
+      <Pressable onPress={() => navigation.navigate('Register')}>
+        <Text >
+          Don’t have an account? Register
+        </Text>
+      </Pressable>
+    </KeyboardAvoidingView>
+  );
 }
