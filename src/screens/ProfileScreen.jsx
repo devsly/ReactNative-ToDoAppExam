be670a0a340a59ce47b10b6b6221e085274f39ac
devsly <68057615+devsly@users.noreply.github.com>
2026-02-05 12:50:53 +0200

    Update ProfileScreen with UI

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,9 +1,36 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, Pressable } from 'react-native';
 
 export default function ProfileScreen() {
+  // временно – по-късно от Firebase Auth
+  const user = {
+    email: 'user@mail.com',
+    displayName: 'Slav Slavov',
+    photoURL: ''
+  };
+
   return (
-     <View>
-      <Text>Profile Screen</Text>
+    <View >
+      <View >
+        <Image source={{ uri: user.photoURL }}  />
+
+        <Text >{user.displayName}</Text>
+        <Text >{user.email}</Text>
+      </View>
+
+      <View >
+        <Pressable >
+          <Text >Change Profile Photo</Text>
+        </Pressable>
+
+        <Pressable >
+          <Text >Edit Profile</Text>
+        </Pressable>
+
+        <Pressable >
+          <Text >Logout</Text>
+        </Pressable>
+      </View>
     </View>
-  )
+  );
 }
+
